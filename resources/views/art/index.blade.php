07b624b68a9adf1e874cbbde0ada8e202ab39522
surelynobody161 <surelynobody161@gmail.com>
2024-10-23 17:08:23 +0200

    navbar aangemaakt met links die nog niet naar de goede paginas verwijzen

--- resources/views/art/index.blade.php
+++ resources/views/art/index.blade.php
@@ -2,24 +2,38 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
 
+<nav>
     <ul>
-        @foreach($arts as $art)
-            <div>
-                <h2>{{ $art->title }}</h2>
-                <p>{{ $art->description }}</p>
 
-                <img src="{{ asset('storage/' . $art->art) }}" alt="{{ $art->title }}" style="max-width: 200px; height: auto;">
-            </div>
-        @endforeach
-
+        @if (auth()->check())
+            <li>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            </li>
+        @else
+            <li><a href="{{ route('login') }}">Login</a></li>
+            <li><a href="{{ route('register') }}">Register</a></li>
+        @endif
     </ul>
+</nav>
+
+<ul>
+    @foreach($arts as $art)
+        <div>
+            <h2>{{ $art->title }}</h2>
+            <p>{{ $art->description }}</p>
+            <img src="{{ asset('storage/' . $art->art) }}" alt="{{ $art->title }}" style="max-width: 200px; height: auto;">
+        </div>
+    @endforeach
+</ul>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArtsController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
@@ -30,8 +32,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('/categories', CategoryController::class);
+    Route::post('/logout', [CheckerController::class, 'destroy'])->name('logout');
+
 });
-Route::resource('/arts', controller: \App\Http\Controllers\ArtsController::class);
+Route::resource('/arts', controller: ArtsController::class);
+Route::get('/login', [CheckerController::class, 'create'])->name('login');
+
+Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+
 
 
 require __DIR__.'/auth.php';
